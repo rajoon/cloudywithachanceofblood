6a18f856f241f40d369371ad572658713c652f67
rajoon <shem@rajoon.com>
2015-05-19 13:27:33 -0400

    Updated project, Giphy Streaming API limit reached for my IP

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,5 +1,5 @@
 $(function() {
-    var xhr = $.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=gore+blood");
+    var xhr = $.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=blood+gore");
     xhr.done(function(data) { 
       $('body').css('background-image', 'url(' + data.data.image_url + ')');
     });
---
+++ assets/javascript/global.js
@@ -0,0 +1,10 @@
+$( "a.searchOpen" ).click(function() {
+  $( ".search" ).fadeIn( 100);
+  $('input[type=text]').focus();
+  $("a.searchOpen").fadeOut( 100);
+  });
+
+$( "a.searchClose" ).click(function() {
+  $( ".search" ).fadeOut( 100);
+  $("a.searchOpen").fadeIn( 100);
+  });
